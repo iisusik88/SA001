baa90efbd46c42cbe4349a91464e691420fe90e4
iisusik88 <iisusik@bk.ru>
2025-11-17 16:42:49 +0600

    Create testScript.py

---
+++ testScript.py
@@ -0,0 +1,7 @@
+a = float(input("Введите первое число: "))
+b = float(input("Введите второе число: "))
+
+print(f"{a} + {b} = {a + b}")
+print(f"{a} - {b} = {a - b}")
+print(f"{a} * {b} = {a * b}")
+print(f"{a} / {b} = {a / b}")
